295cbe50dfb3366e66e91b6df5b957ddc7c8809f
iamd26 <54445875+iamd26@users.noreply.github.com>
2025-09-06 19:15:59 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('tp-cache-v1').then(cache => cache.addAll([
+    caches.open('tp-cache-v3').then(cache => cache.addAll([
       './',
       './index.html',
       './manifest.webmanifest',
@@ -13,7 +13,7 @@ self.addEventListener('install', event => {
 });
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== 'tp-cache-v1').map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== 'tp-cache-v3').map(k => caches.delete(k))))
     .then(() => self.clients.claim())
   );
 });
