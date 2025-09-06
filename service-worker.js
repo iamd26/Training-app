0d4f8f789c38257962eab3fed7f6b855559ceb4b
iamd26 <54445875+iamd26@users.noreply.github.com>
2025-09-06 16:55:14 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('tp-cache-v5').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== 'tp-cache-v5').map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
